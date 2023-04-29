6f158f1e3dc553e2d5b296673752f85c62b46ac8
Pawel <p.plichta00@gmail.com>
2023-04-30 00:33:23 +0200

    add files

--- main.tf
+++ main.tf
@@ -1,9 +1,46 @@
 terraform {
   required_version = ">= 1.0"
-  backend "local" {}
+  backend "s3" {}
 }
-
 
 provider "aws" {
   region = var.region
+}
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"
+
+  name = "eMASE"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_ipv6        = false
+}
+
+resource "aws_security_group" "lb_public_access" {
+  name   = "lb-public-access"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = module.vpc.private_subnets_cidr_blocks
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,21 @@
 variable "region" {
-  description   = "AWS Default Region"
-  type          = string
-  default       = "eu-west-1"
-}  
+  description = "AWS Default Region"
+  type        = string
+  default     = "eu-west-1"
+}
 
 variable "ami_id" {
   description = "ID of the AMI to use by EC2 Instances"
   type        = string
   default     = ""
+
+  validation {
+    
+    condition = length(var.ami_id) >= 12
+    error_message = "AMI ID should be at least 12 characters long"
+  }
+
+
 }
 
 
@@ -15,3 +23,5 @@ variable "instances_per_subnet" {
   description = "Count of EC2 Instances to create in each Subnet"
   type        = number
 }
+
+
